56033d1e76d146b7b692c832bc13f237f12ef9a6
freakyclown <freakyclown@gmail.com>
2015-01-03 14:49:03 +0000

    Create X-Powered-By_header_checker.js

---
+++ passive/X-Powered-By_header_checker.js
@@ -0,0 +1,25 @@
+// X-Powered-By finder by freakyclown@gmail.com
+
+function scan(ps, msg, src) 
+{
+
+    alertRisk = 0
+    alertReliability = 2
+    alertTitle = "Server Leaks Information via 'X-Powered-By' HTTP Response Header Field(s)(script)"
+    alertDesc = "The web/application server is leaking information via one or more 'X-Powered-By' HTTP response headers. Access to such information may facilitate attackers identifying other frameworks/components your web application is reliant upon and the vulnerabilities such components may be subject to."
+    alertSolution = "Ensure that your web server, application server, load balancer, etc. is configured to suppress 'X-Powered-By' headers."
+
+    cweId = 200
+    wascId = 13
+
+    url = msg.getRequestHeader().getURI().toString();
+
+    if (msg) 
+	{
+	  if (headers = msg.getResponseHeader().getHeaders("X-Powered-By"));
+		{
+       	 ps.raiseAlert(alertRisk, alertReliability, alertTitle, alertDesc, url, '', '', '', alertSolution,headers, cweId, wascId, msg);
+		}     
+	}
+    
+}
