8260986ab6557537f5dc1c6f58a0a352715066cd
freakyclown <freakyclown@gmail.com>
2015-01-28 22:12:36 +0000

    Update Find HTML Comments.js
    reinstated rightful script owner

--- passive/Find HTML Comments.js
+++ passive/Find HTML Comments.js
@@ -1,4 +1,22 @@
-// html comment finder by freakyclown@gmail.com
+// The scan function will be called for request/response made via ZAP, excluding some of the automated tools
+// Passive scan rules should not make any requests 
+
+// Note that new passive scripts will initially be disabled
+// Right click the script in the Scripts tree and select "enable"  
+
+// PassiveHTMLCommentFinder.js
+// kingthorin+owaspzap@gmail.com
+
+// References:
+// RegEx Testing: http://regex101.com/r/tX1hS1
+// Initial discussion: https://groups.google.com/forum/#!topic/zaproxy-develop/t-1-yI7iErw\
+// RegEx adapted from work by Stephen Ostermiller: http://ostermiller.org/findhtmlcomment.html
+// Tweak to RegEx provided by thc202
+
+// NOTE: Designed to work with 2.2 Weekly build version D-2014-03-10 or stable builds at or above v2.3
+// NOTE: This script ONLY finds HTML comments. It DOES NOT find JavaScript or other comments.
+// NOTE: This script will only find HTML comments in content which passes through ZAP. 
+//		Therefore if you browser is caching you may not see something you expect to.
 
 function scan(ps, msg, src) {
 	// lets set up some details we will need for alerts later if we find some comments
