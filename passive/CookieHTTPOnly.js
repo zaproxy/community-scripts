73d7ac7a9feaa70292ecc5e545112764a4709d73
freakyclown <freakyclown@gmail.com>
2015-03-31 08:34:33 +0000

    Update CookieHTTPOnly.js
    spelling error correction

--- passive/CookieHTTPOnly.js
+++ passive/CookieHTTPOnly.js
@@ -5,7 +5,7 @@ function scan(ps, msg, src)
 
     alertRisk = 1
     alertReliability = 2
-    alertTitle = "Cookie set withough HTTPOnly Flag(script)"
+    alertTitle = "Cookie set without HTTPOnly Flag(script)"
     alertDesc = "A cookie has been set without the HttpOnly flag, which means that the cookie can be accessed by JavaScript. If a malicious script can be run on this page then the cookie will be accessible and can be transmitted to another site. If this is a session cookie then session hijacking may be possible."
     alertSolution = "Ensure that the HttpOnly flag is set for all cookies."
 
