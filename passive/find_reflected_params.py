a25d50eb71a94ad027ffb027bf45e9cf0146b087
mar adrian belen <maradrianbelen@gmail.com>
2017-04-18 17:27:19 +0800

    Update find_reflected_params.py
    improve search, and follow good programming practices

--- passive/find_reflected_params.py
+++ passive/find_reflected_params.py
@@ -8,41 +8,39 @@ Passive scan rules should not make any requests
 
 Note that new passive scripts will initially be disabled
 Right click the script in the Scripts tree and select "enable"
+
 """  
-def compare(paramvalue_pair,msg): #get the value in a single parameter and compare it on the HTTP response.
+def compare(paramvalue_pair,msg): 
 	value=paramvalue_pair.split('=')
-	#print 'parameter ' + value[0]+ ' is equal to ' + value[1]
-	# add scan for header
 	body=msg.getResponseBody().toString()
 	header=msg.getResponseHeader().toString();
 	reflected=''
-	if body.find(value[1])>-1 or header.find(value[1])>-1:
+	if (value[1] in body or value[1] in header) and value[1] != '': 
 		reflected=value[0]
 	return reflected
-	
 
-
-def scan(ps, msg, src):
+def scan(ps, msg, src): 
+	alertTitle='Reflected HTTP GET parameter(s)'
+	alertDesc='Reflected parameter value has been found. A reflected parameter values may introduce XSS vulnerability or HTTP header injection.'
 	reflected_params=''
 	URI=msg.getRequestHeader().getURI();
-	query=msg.getRequestHeader().getURI().getQuery();
-	print 'params of ' + URI.toString();
-	print '\n'
-	#get a LIST of  param:value pairs. i.e test=ddd&ddd=sdsd
-	if msg.getRequestHeader().getURI().getQuery():
-		uriofreflected_param=msg.getRequestHeader().getURI().toString()
-		paramvalue_pair=query.split('&');#test=ddd
+	query=URI.getQuery();
+	if query:
+		print query
+		uriofreflected_param=URI.toString()
+		paramvalue_pair=query.split('&');
 		i=0;
-		while(i<len(paramvalue_pair)): # send a single param:value pair.
+		while(i<len(paramvalue_pair)):
 			if(compare(paramvalue_pair[i],msg)):
-				reflected_params=reflected_params + ',' + compare(paramvalue_pair[i],msg)
+				reflected_params=reflected_params  + compare(paramvalue_pair[i],msg) + ','
 			i=i+1;
 		if(reflected_params):
-			ps.raiseAlert(0, 2, 'Find reflected parameter values', 'Reflected parameter value has been found. A reflected parameter values may introduce XSS vulnerability or HTTP header injection.',
-			uriofreflected_param,
-			'Reflected Parameters: ' + reflected_params, 'blank', 'blank', '', '', 0, 0, msg);
-	else:
-		print URI.toString() + ' has no parameter'
+			ps.raiseAlert(0, 2, alertTitle, alertDesc, uriofreflected_param, reflected_params, '', '', '', '', 0, 0, msg);
 
-# line 36-40. the search code does not work properly, and it only shows the last reflected parameter if there is multiple reflected parameters in one HTTP REQUST
-# I am wrong. the reflected_params parameter is incremented when a reflected parameter is found.
+
+
+
+
+
+
+
