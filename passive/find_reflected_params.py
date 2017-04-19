74f5d27e0f30fa1e268a14d6390aed66e8fddca2
mar adrian belen <maradrianbelen@gmail.com>
2017-04-19 12:51:44 +0800

    Update find_reflected_params.py
    fix that :)

--- passive/find_reflected_params.py
+++ passive/find_reflected_params.py
@@ -1,22 +1,19 @@
 """
-
 looks for parameter values that are reflected in the response.
 Author: maradrianbelen.com
-
 The scan function will be called for request/response made via ZAP, excluding some of the automated tools
 Passive scan rules should not make any requests 
-
 Note that new passive scripts will initially be disabled
 Right click the script in the Scripts tree and select "enable"
-
 """  
 def compare(paramvalue_pair,msg): 
-	value=paramvalue_pair.split('=')
-	body=msg.getResponseBody().toString()
-	header=msg.getResponseHeader().toString();
 	reflected=''
-	if (value[1] in body or value[1] in header) and value[1] != '': 
-		reflected=value[0]
+	if len(paramvalue_pair.split('=',1))>1:
+		value=paramvalue_pair.split('=',1)
+		body=msg.getResponseBody().toString()
+		header=msg.getResponseHeader().toString()
+		if (value[1] in body or value[1] in header) and value[1] != '' and len(value[1])>7: 
+			reflected=value
 	return reflected
 
 def scan(ps, msg, src): 
@@ -26,21 +23,12 @@ def scan(ps, msg, src):
 	URI=msg.getRequestHeader().getURI();
 	query=URI.getQuery();
 	if query:
-		print query
 		uriofreflected_param=URI.toString()
 		paramvalue_pair=query.split('&');
 		i=0;
 		while(i<len(paramvalue_pair)):
 			if(compare(paramvalue_pair[i],msg)):
-				reflected_params=reflected_params  + compare(paramvalue_pair[i],msg) + ','
+				reflected_params=reflected_params  + compare(paramvalue_pair[i],msg)[0] + ','
 			i=i+1;
 		if(reflected_params):
 			ps.raiseAlert(0, 2, alertTitle, alertDesc, uriofreflected_param, reflected_params, '', '', '', '', 0, 0, msg);
-
-
-
-
-
-
-
-
