649bd92b8fbe9707800930af19f6217de39ac395
Alessandro Pezzè <igougi.ui@gmail.com>
2018-03-02 10:56:56 +0100

    Create dropCookiesSelectively.js
    Add a simple example to drop from Requests selected cookies

---
+++ proxy/dropCookiesSelectively.js
@@ -0,0 +1,23 @@
+//@zaproxy-proxy
+
+globalCookies = ['CONSENT', 'GZ', 'SID']
+
+function proxyRequest(msg) {
+    globalCookies.forEach(function (cookieToDrop) {
+        cookies = msg.getRequestHeader().getHttpCookies() // This is a List<HttpCookie>
+        iterator = cookies.iterator()
+        while (iterator.hasNext()) {
+            cookie = iterator.next() // This is a HttpCookie
+            if (cookie.name == cookieToDrop) {
+                iterator.remove()
+                print('removed' + cookie.name)
+            }
+        }
+        msg.getRequestHeader().setCookies(cookies)
+    })
+    return true
+}
+
+function proxyResponse(msg) {
+    return true
+}
