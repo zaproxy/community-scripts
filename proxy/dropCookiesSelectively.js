922c9b8de8fa4142bd403ae7eaba416289992af8
Alessandro Pezzè <igougi.ui@gmail.com>
2018-03-03 11:51:55 +0100

    Update dropCookiesSelectively.js

--- proxy/dropCookiesSelectively.js
+++ proxy/dropCookiesSelectively.js
@@ -3,18 +3,16 @@
 globalCookies = ['CONSENT', 'GZ', 'SID']
 
 function proxyRequest(msg) {
-    globalCookies.forEach(function (cookieToDrop) {
-        cookies = msg.getRequestHeader().getHttpCookies() // This is a List<HttpCookie>
-        iterator = cookies.iterator()
-        while (iterator.hasNext()) {
-            cookie = iterator.next() // This is a HttpCookie
-            if (cookie.name == cookieToDrop) {
-                iterator.remove()
-                print('Stripped away: ' + cookie.name)
-            }
+    cookies = msg.getRequestHeader().getHttpCookies() // This is a List<HttpCookie>
+    iterator = cookies.iterator()
+    while (iterator.hasNext()) {
+        cookie = iterator.next() // This is a HttpCookie
+        if (globalCookies.indexOf(cookie.name) > -1) {
+            iterator.remove()
+            print('Stripped away: ' + cookie.name)
         }
-        msg.getRequestHeader().setCookies(cookies)
-    })
+    }
+    msg.getRequestHeader().setCookies(cookies)
     return true
 }
 
