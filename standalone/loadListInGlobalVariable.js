7054056d17ec6fabe7c1b2dc0e2ff0dcef63b7c9
Alessandro Pezzè <igougi.ui@gmail.com>
2018-03-03 11:46:09 +0100

    Update loadListInGlobalVariable.js

--- standalone/loadListInGlobalVariable.js
+++ standalone/loadListInGlobalVariable.js
@@ -1,13 +1,8 @@
 //@zaproxy-standalone
 
-/*
-Note that the script below will work only on Zaproxy > 2.7.0
-Since .getGlobalVars() is not supported on previous versions
-*/
-
 org.zaproxy.zap.extension.script.ScriptVars.setGlobalVar('LIST', JSON.stringify(['Zaproxy', 'Zap', 'Simon', 'Mozilla']))
 
-list = JSON.parse(org.zaproxy.zap.extension.script.ScriptVars.getGlobalVars()['LIST'])
+list = JSON.parse(org.zaproxy.zap.extension.script.ScriptVars.getGlobalVar('LIST'))
 
 list.forEach(function(item) {
 	print(item)
