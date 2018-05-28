b38b5513e5c935d7e4ec2ba0effc09bad146995b
Alessandro Pezzè <igougi.ui@gmail.com>
2018-05-28 14:05:57 +0200

    Create export.js

---
+++ standalone/export.js
@@ -0,0 +1,10 @@
+//@zaproxy-standalone
+
+// Every function in this file will be imported and added in the global context in ./import.js
+
+var number = 1
+
+function customFunction(customParam) {
+    print(customParam)
+    return 'the return'
+}
