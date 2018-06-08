6585ef2d30f3907acae1aa3ebc4aedfccab46b46
Pamplemousse <xav.maso@gmail.com>
2018-06-08 09:59:38 +0100

    Check that initiator of the request was the proxy
    There is no point in injecting JS into a response coming back from a
    spider for example...

--- httpsender/inject_js_in_html_page.js
+++ httpsender/inject_js_in_html_page.js
@@ -21,13 +21,14 @@ function loadScriptFromFile(file) {
 function sendingRequest(msg, initiator, helper) {}
 
 function responseReceived(msg, initiator, helper) {
+    if (initiator == -1) { return; }
+
     body = msg.getResponseBody();
     bodyAsStr = body.toString();
     header = msg.getResponseHeader();
 
     xRequestedWith = msg.getRequestHeader().getHeader('X-Requested-With');
     contentType = header.getHeader('Content-Type');
-
     contentTypeRegex = new RegExp(/text\/html;/g);
     indexOfHead = bodyAsStr.indexOf('<head>');
 
