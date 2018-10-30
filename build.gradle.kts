c88a52fe3e4e2727a66492e91ce8d06854995c3c
thc202 <thc202@gmail.com>
2018-10-30 22:50:17 +0000

    Update Gradle to 4.10.2
    Update Gradle and Spotless to 3.15.0.
    Update to latest Kotlin DSL:
     - Use `getByName` to configure the `wrapper` task.
     - Use `sourceSets` directly.
    
    Extract variable for JUnit version.
    Add a comment to clarify the expected versions of the scripting
    libraries.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     `java-library`
-    id("com.diffplug.gradle.spotless") version "3.14.0"
+    id("com.diffplug.gradle.spotless") version "3.15.0"
 }
 
 tasks {
-    "wrapper"(Wrapper::class) {
-        gradleVersion = "4.9"
+    getByName<Wrapper>("wrapper") {
+        gradleVersion = "4.10.2"
         distributionType = Wrapper.DistributionType.ALL
     }
 }
@@ -14,16 +14,19 @@ repositories {
     mavenCentral()
 }
 
+val jupiterVersion = "5.2.0"
+
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.2.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$jupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jupiterVersion")
 
     testImplementation("org.assertj:assertj-core:3.11.0")
     testImplementation("org.apache.commons:commons-lang3:3.8")
 
     testRuntimeOnly("org.zaproxy:zap:2.7.0")
 
+    // The following versions should match the ones of the add-ons.
     testImplementation("org.codehaus.groovy:groovy-all:2.4.14")
     testImplementation("org.jruby:jruby-complete:1.7.4")
     testImplementation("org.python:jython-standalone:2.7.1")
@@ -68,13 +71,9 @@ val syncScriptsDirTask by tasks.creating(Sync::class) {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-
-    sourceSets {
-        "main" {
-            output.dir(mapOf("builtBy" to syncScriptsDirTask), scriptsDir)
-        }
-    }
 }
+
+sourceSets["main"].output.dir(mapOf("builtBy" to syncScriptsDirTask), scriptsDir)
 
 spotless {
     java {
