04978492ffbd08eccb43c3377c6ef302e6b9ddc5
Simon Bennetts <psiinon@gmail.com>
2020-04-17 16:05:37 +0100

    Added juice shop client auth scripts.
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

---
+++ standalone/Juice shop authentication by form.js
@@ -0,0 +1,23 @@
+// Standalone script to demonstrate logging into OWASP Juice Shop (https://owasp.org/www-project-juice-shop/) 
+// via the standard login form using Firefox.
+// Juice Shop will need to be accessible via http://localhost:3000/ and you will need to register
+// a user with a name of test@test.com and a password of test123
+// You can change any of the variables to match your environment if needed.
+
+var By = Java.type('org.openqa.selenium.By');
+var Thread = Java.type('java.lang.Thread');
+var juiceshop = 'http://localhost:3000/';
+var username = 'test@test.com';
+var password = 'test123';
+
+var extSel = org.parosproxy.paros.control.Control.getSingleton().
+				getExtensionLoader().getExtension(
+					org.zaproxy.zap.extension.selenium.ExtensionSelenium.class) 
+
+var wd = extSel.getWebDriverProxyingViaZAP(1, "firefox");
+wd.get(juiceshop);
+Thread.sleep(1000);
+wd.get(juiceshop + '#/login');
+wd.findElement(By.id("email")).sendKeys(username);
+wd.findElement(By.id("password")).sendKeys(password);
+wd.findElement(By.id("loginButton")).click();
---
+++ standalone/Juice shop authentication by google.js
@@ -0,0 +1,27 @@
+// Standalone script to demonstrate logging into OWASP Juice Shop (https://owasp.org/www-project-juice-shop/) 
+// via Google SSO using Firefox.
+// Juice Shop will need to be accessible via http://localhost:3000/ and you will need to change the 
+// username and password to match a valid Google account.
+
+var By = Java.type('org.openqa.selenium.By');
+var Thread = Java.type('java.lang.Thread');
+var juiceshop = 'http://localhost:3000/';
+var username = 'zap.addo.sb@gmail.com'; // Change this to an account you own
+var password = 'nottherealpassword';	// Change this to the right password for your account
+
+var extSel = org.parosproxy.paros.control.Control.getSingleton().
+				getExtensionLoader().getExtension(
+					org.zaproxy.zap.extension.selenium.ExtensionSelenium.class) 
+
+var wd = extSel.getWebDriverProxyingViaZAP(1, "firefox");
+wd.get(juiceshop);
+Thread.sleep(1000);
+wd.get(juiceshop + '#/login');
+Thread.sleep(1000);
+wd.findElement(By.id("loginButtonGoogle")).click();
+Thread.sleep(1000);
+wd.findElement(By.id("identifierId")).sendKeys(username);
+wd.findElement(By.className("RveJvd")).click();
+Thread.sleep(1000);
+wd.findElement(By.name("password")).sendKeys(password);
+wd.findElement(By.className("RveJvd")).click();
