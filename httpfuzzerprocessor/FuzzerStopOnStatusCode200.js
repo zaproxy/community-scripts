ab429d409dca01b1671f64ecbd9ee47e8b2c70ac
Thomas <johanns.thomas@gmail.com>
2020-06-01 15:46:21 +0200

    Removed JsDoc

--- httpfuzzerprocessor/FuzzerStopOnStatusCode200.js
+++ httpfuzzerprocessor/FuzzerStopOnStatusCode200.js
@@ -1,23 +1,6 @@
-/**
- * Processes the fuzzed message (payloads already injected).
- * 
- * Called before forwarding the message to the server.
- * 
- * @param {HttpFuzzerTaskProcessorUtils} utils - A utility object that contains functions that ease common tasks.
- * @param {HttpMessage} message - The fuzzed message, that will be forward to the server.
- */
 function processMessage(utils, message) {
 }
 
-/**
- * Processes the fuzz result.
- * 
- * Called after receiving the fuzzed message from the server.
- * 
- * @param {HttpFuzzerTaskProcessorUtils} utils - A utility object that contains functions that ease common tasks.
- * @param {HttpFuzzResult} fuzzResult - The result of sending the fuzzed message.
- * @return {boolean} Whether the result should be accepted, or discarded and not shown.
- */
 function processResult(utils, fuzzResult){
 	if (fuzzResult.getHttpMessage().getResponseHeader().getStatusCode() == 200)
 		utils.stopFuzzer();
