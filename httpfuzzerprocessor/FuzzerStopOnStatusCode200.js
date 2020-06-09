d562ee35e52ffc00727c80341a85a8d0e46cd861
Thomas <johanns.thomas@gmail.com>
2020-06-09 18:58:16 +0200

    Added parameter

--- httpfuzzerprocessor/FuzzerStopOnStatusCode200.js
+++ httpfuzzerprocessor/FuzzerStopOnStatusCode200.js
@@ -1,8 +1,14 @@
+var STATUS_CODE_PARAM = "Status Code"
+
 function processMessage(utils, message) {
 }
 
 function processResult(utils, fuzzResult){
-	if (fuzzResult.getHttpMessage().getResponseHeader().getStatusCode() == 200)
+	if (fuzzResult.getHttpMessage().getResponseHeader().getStatusCode() == utils.getParameters().get(STATUS_CODE_PARAM))
 		utils.stopFuzzer();
 	return true;
+}
+
+function getRequiredParamsNames(){
+	return [STATUS_CODE_PARAM]
 }
