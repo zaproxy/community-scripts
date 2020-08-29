d997806a1d65881b516a741b47d2c70b04617220
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-08-29 02:31:53 +0200

    Update active/SSTI.py
    Co-authored-by: kingthorin <kingthorin@users.noreply.github.com>

--- active/SSTI.py
+++ active/SSTI.py
@@ -6,13 +6,7 @@ github : https://github.com/knassar702/scant3r
 
 """
 def scanNode(sas, msg):
-  # Copy requests before reusing them
-  msg = msg.cloneRequest();
-
-  # sendAndReceive(msg, followRedirect, handleAntiCSRFtoken)
-  sas.sendAndReceive(msg, False, False);
-
-  # Test the responses and raise alerts as below
+  pass
 
 ssti_payloads = {
     'abcd{{6*6}}123':'abcd36123',
