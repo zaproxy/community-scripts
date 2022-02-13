9919e35db9a67f448fe656330527c38117bd3468
hahwul <hahwul@gmail.com>
2022-02-13 14:06:55 +0900

    Add httpfuzzerprocessor/addCacheBusting.js
    Signed-off-by: hahwul <hahwul@gmail.com>

---
+++ httpfuzzerprocessor/addCacheBusting.js
@@ -0,0 +1,39 @@
+function processMessage(utils, message) {
+	var cbValue = "cbvalue"+Math.floor(Math.random() * 10000)
+     var updatedUrlParams = setCacheBusting(message,cbValue);
+	message.getRequestHeader().setHeader("X-Cache-Busting", cbValue);
+}
+
+function setCacheBusting(message,cbValue) {
+    var URI = Java.type("org.apache.commons.httpclient.URI");
+    var params = message.getUrlParams();
+    var iterator = params.iterator();
+
+    if(iterator.hasNext()) {
+      var uri = message.getRequestHeader().getURI().toString() + "&cbvalue="+cbValue
+	 message.getRequestHeader().setURI(new URI(uri, false))
+    } else {
+      var uri = message.getRequestHeader().getURI().toString() + "?cbvalue="+cbValue
+	 message.getRequestHeader().setURI(new URI(uri, false))
+    }
+
+    whil(iterator.hasNext()) {
+        var param = iterator.next();
+        if (param.getName().equals("cachebusting")) {
+		param.setValue(cbValue)
+        }
+    }
+    return params;
+}
+
+function processResult(utils, fuzzResult){
+	return true;
+}
+
+function getRequiredParamsNames(){
+	return [];
+}
+
+function getOptionalParamsNames(){
+	return [];
+}
