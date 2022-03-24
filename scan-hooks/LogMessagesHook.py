a018cef25846f5c956c8c0fff7b5a61bbef55390
Simon Bennetts <psiinon@gmail.com>
2022-03-24 16:00:33 +0000

    Add scripts to help debugging in docker
    Signed-off-by: Simon Bennetts <psiinon@gmail.com>

---
+++ scan-hooks/LogMessagesHook.py
@@ -0,0 +1,9 @@
+# A scan hook (https://www.zaproxy.org/docs/docker/scan-hooks/) which adds a script for logging all requests.
+# To use this script copy it and the httpsender/LogRequests.js script to your CWD.
+# Then run ZAP like this:
+#     docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t https://www.example.com --hook=LogMessagesHook.py
+# The requests and responses should be written to a req-resp-log.txt file in the CWD.
+
+def zap_started(zap, target):
+  zap.script.load('LogMessages.js', 'httpsender', 'Oracle Nashorn', '/zap/wrk/LogMessages.js')
+  zap.script.enable('LogMessages.js')
