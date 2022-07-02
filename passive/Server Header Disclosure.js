c6899e6349f77d037e3884d657d48b2a2c3ec61d
prateekrana-getastra <prateek.rana@getastra.com>
2022-07-02 20:14:55 +0530

    Revert "added function to report leaked server version from header field"
    This reverts commit 29f2f7dba20035c8b23ade39ce3a8c3c6c24242e.
    
    Signed-off-by: prateekrana-getastra <prateek.rana@getastra.com>

--- passive/Server Header Disclosure.js
+++ passive/Server Header Disclosure.js
@@ -1,5 +1,4 @@
 // Server Header Check by freakyclown@gmail.com
-// Server Version leak via header field by prateek.rana@getastra.com
 
 function scan(ps, msg, src) 
 {
@@ -15,38 +14,10 @@ function scan(ps, msg, src)
 
     var url = msg.getRequestHeader().getURI().toString();
     var headers = msg.getResponseHeader().getHeaders("Server")
-    var headers_string = headers.toString();
     
-    if (headers != null && ExtAlert(headers))
+    if (headers != null)
     {
-        ps.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDesc, url, '', '', '', alertSolution, headers_string, cweId, wascId, msg);
+        ps.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDesc, url, '', '', '', alertSolution,headers, cweId, wascId, msg);
     }
     
-}
-
-function ExtAlert(content) {
-
-    var ext = new RegExp("(\\d+\\.)+\\d+");
-
-    try {
-        var res = ext.exec(content);
-        if (res == null){
-            return false;
-        }
-        res = res.join('');
-
-        if (res === "") {
-            return false;
-        }
-        
-        else {
-            print("Server version leak found via header.");
-            return true;        
-        }
-    }
-
-    catch (err) {
-        print(err);
-        return false;
-    }
 }
