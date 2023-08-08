db10f0ade936a2b5f8e71d923191962cbdfbed11
thc202 <thc202@gmail.com>
2023-08-08 14:25:16 +0100

    Update Windows installation paths
    Update to match the newer location.
    
    Related to zaproxy/zaproxy#7996.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- other/af-plans/juiceshop-selenium-auth/js-auth.bat
+++ other/af-plans/juiceshop-selenium-auth/js-auth.bat
@@ -4,4 +4,4 @@ set JS_PWD=test123
 
 # Customize the paths to the zap.bat file and the automation plan to match your environment
 
-C:\Program Files\OWASP\Zed Attack Proxy\zap.bat -cmd -autorun \full\path\juiceshop-auth.yaml
+C:\Program Files\ZAP\Zed Attack Proxy\zap.bat -cmd -autorun \full\path\juiceshop-auth.yaml
