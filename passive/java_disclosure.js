f0f52f0fc6e79c5df9591c62f52a50b14834e7e2
ChieftainY2k <ChieftainY2k@gmail.com>
2023-08-23 09:53:05 +0200

    Passive scan for Java error messages containing sensitive information (CWE-209)
    Signed-off-by: ChieftainY2k <ChieftainY2k@gmail.com>

---
+++ passive/java_disclosure.js
@@ -0,0 +1,31 @@
+//Passive scan for Java error messages containing sensitive information (CWE-209)
+
+function scan(ps, msg, src) {
+    var alertRisk = 2
+    var alertConfidence = 3
+    var alertTitle = 'Java stack trace disclosure (or similar) - investigation required (script)'
+    var alertDesc = 'Java stack trace disclosure (or similar) was found'
+    var alertSolution = 'Investigate Java stack trace disclosures found in the response, remove or mask as required'
+    var cweId = 209
+    var wascId = 0
+
+    var re = /springframework|\.java|rootBeanClass/ig
+
+    var url = msg.getRequestHeader().getURI().toString()
+
+    var contentType = msg.getResponseHeader().getHeader("Content-Type")
+    var unwantedFileTypes = ['image/png', 'image/jpeg','image/gif','application/x-shockwave-flash','application/pdf']
+
+    if (unwantedFileTypes.indexOf(""+contentType) >= 0) {
+        return
+    }
+
+    var body = msg.getResponseBody().toString()
+    if (re.test(body)) {
+        re.lastIndex = 0 // After testing reset index
+        ps.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDesc, url, '', '', body, alertSolution, body, cweId, wascId, msg)
+
+        console.log("Java leak detected");
+    }
+
+}
